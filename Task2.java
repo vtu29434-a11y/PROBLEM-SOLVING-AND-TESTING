5aac60eb79d18be67a1f7e9b3030a110e45ba073
vtu29434-a11y <vtu29434@veltech.edu.in>
2026-07-28 11:22:31 +0530

    Add files via upload

---
+++ Task2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] a = {10, 20, 30, 40, 50};
+        int i = sc.nextInt();
+        System.out.println(a[i]);
+    }
+}
