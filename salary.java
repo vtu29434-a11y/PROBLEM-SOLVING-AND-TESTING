9e8adec4164b854f5c1e44522db6c6b436c5927d
vtu29434-a11y <vtu29434@veltech.edu.in>
2026-08-03 12:01:59 +0530

    Add files via upload

---
+++ salary.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class salary{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+List<Integer>salaries=new ArrayList<>();
+for(int i=0;i<n;i++){
+salaries.add(sc.nextInt());
+}
+List<Integer>updatedSalaries=salaries.stream()
+.map(salary->(int)(salary*1.10))
+.collect(Collectors.toList());
+for(int salary:updatedSalaries){
+System.out.print(salary+" ");
+}
+sc.close();
+}
+}
