3a87001ff0c4724719b84011174a367d727fac00
vtu29434-a11y <vtu29434@veltech.edu.in>
2026-08-10 12:29:37 +0530

    Add files via upload

---
+++ week 3/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class solution {
+
+    public static String findDay(int month, int day, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day); // month is 0-based in Calendar
+
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch (dayOfWeek) {
+            case Calendar.SUNDAY: return "SUNDAY";
+            case Calendar.MONDAY: return "MONDAY";
+            case Calendar.TUESDAY: return "TUESDAY";
+            case Calendar.WEDNESDAY: return "WEDNESDAY";
+            case Calendar.THURSDAY: return "THURSDAY";
+            case Calendar.FRIDAY: return "FRIDAY";
+            case Calendar.SATURDAY: return "SATURDAY";
+            default: return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        int year = sc.nextInt();
+        sc.close();
+
+        System.out.println(findDay(month, day, year));
+    }
+}
