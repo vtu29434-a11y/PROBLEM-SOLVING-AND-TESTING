666bf7f7cbf00f950641a795a3f2a072580b0166
vtu29434-a11y <vtu29434@veltech.edu.in>
2026-08-17 11:19:34 +0530

    Format dayofweek.java with consistent imports

--- week 3/dayofweek.java
+++ week 3/dayofweek.java
@@ -1,11 +1,9 @@
 import java.time.LocalDate;
 import java.time.DayOfWeek;
-
 class Solution {
     public String dayOfTheWeek(int day, int month, int year) {
         LocalDate date = LocalDate.of(year, month, day);
         DayOfWeek dow = date.getDayOfWeek(); 
-        // Convert to string with first letter capitalized
         String result = dow.toString().substring(0,1) + dow.toString().substring(1).toLowerCase();
         return result;
     }
