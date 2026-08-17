bdfc0b6096644807191e642f45eb1be1539e2d93
vtu29434-a11y <vtu29434@veltech.edu.in>
2026-08-17 11:19:54 +0530

    Remove unnecessary comments from localdate.java

--- week 3/localdate.java
+++ week 3/localdate.java
@@ -5,11 +5,9 @@ class Solution {
         LocalDate d = LocalDate.parse(date); 
         return d.getDayOfYear();
     }
-
-    // Quick test
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.dayOfYear("2019-01-09")); // Output: 9
-        System.out.println(sol.dayOfYear("2019-02-10")); // Output: 41
+        System.out.println(sol.dayOfYear("2019-01-09")); 
+        System.out.println(sol.dayOfYear("2019-02-10")); 
     }
 }
