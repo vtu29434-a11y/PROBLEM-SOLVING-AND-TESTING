619cca929f11b369f1bcef556673251a8b4aa9f1
vtu29434-a11y <vtu29434@veltech.edu.in>
2026-08-17 11:22:55 +0530

    Update solution (2).java

---
+++ week 3/solution (2).java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> pq =
+            new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+
+        pq.addAll(freq.entrySet());
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry<Character, Integer> entry = pq.poll();
+            char c = entry.getKey();
+            int count = entry.getValue();
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.frequencySort("tree"));    
+        System.out.println(sol.frequencySort("cccaaa"));  
+        System.out.println(sol.frequencySort("Aabb"));    
+    }
+}
